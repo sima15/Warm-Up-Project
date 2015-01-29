ae777589ee1b863ddf9aed9aece8fa028e004b20
Dwight <d.bennett@aggiemail.usu.edu>
2015-01-28 19:08:31 -0700

    testing again

---
+++ Warm Up Project/src/Driver.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+
+
+public class Driver 
+{
+	public static void main(String[] args) throws IOException
+	{
+		
+		
+		UDPClient client = new UDPClient(args[0], Integer.parseInt(args[1]));
+		
+		client.SendFile(args[2]);
+		
+	}
+	
+	
+}
