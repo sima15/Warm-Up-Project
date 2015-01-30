fa2d8de23acf84a788251778d816640a3ace9345
Q <Q@144.39.236.64>
2015-01-30 15:06:41 -0700

    Adding Server Driver

--- Warm Up Project/src/DriverServer.java
+++ Warm Up Project/src/DriverServer.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class DriverServer {
 	public static void main(String[] args) throws IOException
 	{
-		UDPServer server = new UDPServer();
-		
+		UDPServer server = new UDPServer(args[0], Integer.parseInt(args[1]));
+		server.receiveFile();
 	}
 }
